22232b3b7b6a6b9d6f5fa1213e2972e4731b1c08
Marchez18 <marchez18@gmail.com>
2025-12-13 21:09:03 +0100

    Update dropbox_manager_PRO.py

--- dropbox_manager_PRO.py
+++ dropbox_manager_PRO.py
@@ -54,7 +54,7 @@ print("🔓 Credenciales cargadas correctamente\n")
 
 SOURCE_FOLDER_NAME = "/Camera Uploads/Test-dng"  # Carpeta origen en Dropbox (sin subcarpetas)
 SOURCE_FOLDER_NAME = "/Camera Uploads/2025"  # Carpeta origen en Dropbox (sin subcarpetas)
-SOURCE_FOLDER_NAME = "/Camera Uploads/2022"  # Carpeta origen en Dropbox (sin subcarpetas)
+SOURCE_FOLDER_NAME = "/Camera Uploads/2021"  # Carpeta origen en Dropbox (sin subcarpetas)
 
 
 # Carpeta destino = origen + "-jpg"
